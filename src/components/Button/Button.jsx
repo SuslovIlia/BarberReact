bbead8b81f3782c1426133757a8f2d8bdeb395e6
SuslovIlia <suslovilia2007@gmail.com>
2025-05-27 18:31:45 +0700

    оррекция кнопки 2

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,9 @@ import styles from "./Button.module.css";
 export default function Button({ text, onClack }) {
   return (
     <>
-      <button onClick={onClack}>{text}</button>
+      <button onClick={onClack} className={styles.button}>
+        {text}
+      </button>
     </>
   );
 }
